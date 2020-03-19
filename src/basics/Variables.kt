eac46ea7715d822894a3f23138825199d5e2ed2a
gustavo <gustavo.maia@b2b.com.pt>
2020-03-19 15:15:20 +0000

    Declaring Variables in Kotlin

---
+++ src/basics/Variables.kt
@@ -0,0 +1,13 @@
+package basics
+
+fun main(args: Array<String>){
+
+    var streetNumber: Int
+    var streetCode: Int = 10
+    var streetName = "High Street"
+
+    //val - o valor não pode ser alterado
+    val zip = "E11 P1"
+
+
+}
