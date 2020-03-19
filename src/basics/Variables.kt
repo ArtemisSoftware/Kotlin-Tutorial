15d83a4519c4a85ba8284871e50e0d89740c066d
gustavo <gustavo.maia@b2b.com.pt>
2020-03-19 15:26:37 +0000

    Working with Basic Types in Kotlin

--- src/basics/Variables.kt
+++ src/basics/Variables.kt
@@ -9,5 +9,27 @@ fun main(args: Array<String>){
     //val - o valor não pode ser alterado
     val zip = "E11 P1"
 
+    val myLong = 10L
+    val myFloat = 10F
+    val myHex = 0x0F
+    val myBinary = 0xb01
 
+
+    //String
+
+    val myChar = 'A'
+    val myString = "My String"
+
+    val escapeCharacters = "A new link \n"
+
+    val multipleLinea ="""
+        This is still a string
+        and this is another line
+        """
+
+    val years = 10
+    val message = "A decade is $years years "
+
+    val name = "Mary"
+    val anotherMessage = "Length of name is ${name.length}"
 }
