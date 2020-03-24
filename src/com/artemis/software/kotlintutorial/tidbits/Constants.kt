a3b18bbc55e853b820635b78a3a0759f73a536b3
gustavo <gustavo.maia@b2b.com.pt>
2020-03-24 10:32:12 +0000

    Declaring Constants

---
+++ src/com/artemis/software/kotlintutorial/tidbits/Constants.kt
@@ -0,0 +1,14 @@
+package com.artemis.software.kotlintutorial.tidbits
+
+val CopyrightAuthor = "Shaka"
+
+
+object Copyright{
+    val author = "Camus"
+}
+
+
+fun main(args: Array<String>){
+
+    Copyright.author
+}
