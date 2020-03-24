4396798d58a33cf7559d75e6741ca40005420479
gustavo <gustavo.maia@b2b.com.pt>
2020-03-24 10:59:38 +0000

    Lambda Expressions

--- src/com/artemis/software/kotlintutorial/functional/GettingFunctional.kt
+++ src/com/artemis/software/kotlintutorial/functional/GettingFunctional.kt
@@ -1,5 +1,7 @@
 package com.artemis.software.kotlintutorial.functional
 
+import java.sql.DatabaseMetaData
+
 fun operation(x: Int, y: Int, op: (Int, Int) -> Int): Int{
     return op(x, y)
 }
@@ -12,10 +14,54 @@ fun route(path: String, vararg actions: (String, String) -> String){
 
 }
 
+fun unaryOp(op: (Int) -> Int){
+
+}
+
+fun unaryOperation(z: Int, op: (Int) -> Int){
+
+}
 fun sum(x: Int, y: Int) = x + y
+
+
+fun transaction(db: Database,  code: () -> Unit){
+
+    try{
+        code()
+    }
+    finally {
+        db.commit()
+    }
+}
+
+class Database {
+    fun commit() {}
+}
 
 
 fun main(args: Array<String>){
 
     println(operation(1,2, ::sum))
+
+    val sumLambda: (Int, Int) -> Int = { x: Int, y: Int -> x + y }
+    operation(1,2, sumLambda)
+
+    unaryOperation(2, {x -> x + x})
+
+    unaryOperation(3, {it * it})
+
+    unaryOperation(3) {it * it}
+
+    unaryOp{
+        it * it
+    }
+
+    val db = Database()
+
+    transaction(db){
+        //interact with the database
+    }
+
+
+    unaryOperation(5, fun(x: Int): Int{ return x * x } )
 }
